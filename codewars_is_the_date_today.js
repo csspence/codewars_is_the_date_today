df72b8448921060916dedec23bb1d2224735ae3f
Crew Spence <csspence123@gmail.com>
2020-12-03 06:09:34 -0600

    Wrote working solution.

--- codewars_is_the_date_today.js
+++ codewars_is_the_date_today.js
@@ -6,7 +6,7 @@ Make sure that your function does not return a false positive by just checking j
 
 const isToday = (date) => {
   let today = new Date();
-  let todayString = today.toString();
-  let dateString = date.toString();
+  let todayString = today.toString().slice(0,14);
+  let dateString = date.toString().slice(0,14);
   return dateString !== todayString ? false : true;
 }
